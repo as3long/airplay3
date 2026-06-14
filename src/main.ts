d0e8c9427f42a08ebb6bce49dc7f14ada85b9399
as3long <as3long@qq.com>
2026-06-14 12:12:19 +0800

    chore: remove unused AboutPage.vue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import AboutPage from './views/AboutPage.vue'
 
 const app = createApp(App)
-app.use(createPinia())
+const pinia = createPinia()
+app.use(pinia)
+
+const route = window.location.hash.replace('#', '') || '/'
+if (route === '/about') {
+  app.component('AboutPage', AboutPage)
+}
+
 app.mount('#app')
