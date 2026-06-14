ef540e4ac53a82890911b2ffa6f645086482d831
as3long <as3long@qq.com>
2026-06-14 12:13:44 +0800

    fix: remove AboutPage import from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import AboutPage from './views/AboutPage.vue'
 
 const app = createApp(App)
-const pinia = createPinia()
-app.use(pinia)
-
-const route = window.location.hash.replace('#', '') || '/'
-if (route === '/about') {
-  app.component('AboutPage', AboutPage)
-}
-
+app.use(createPinia())
 app.mount('#app')
